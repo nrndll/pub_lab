017dca8fe4dfe753f64b114d07fa2507f70363c5
Nathan <nathan.rendall@gmail.com>
2021-06-23 12:11:33 +0100

    increase_till method added to pub class

--- src/pub.py
+++ src/pub.py
@@ -3,3 +3,6 @@ class Pub:
         self.name = name
         self.till = till
         self.drinks = []
+
+    def increase_till(self, amount):
+        self.till += amount
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,11 +4,15 @@ from src.pub import *
 class TestPub(unittest.TestCase):
     
     def setUp(self):
-        self.pub = Pub("Stonefire Tavern", 100.00)
+        self.pub = Pub("Stonefire Tavern", 100)
 
     def test_pub_has_name(self):
         self.assertEqual("Stonefire Tavern", self.pub.name)
 
     def test_pub_has_counter(self):
         self.assertEqual(100, self.pub.till)
+
+    def test_pub_reduce_till(self):
+        self.pub.increase_till(10)
+        self.assertEqual(110, self.pub.till)
 

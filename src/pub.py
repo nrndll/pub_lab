78ae268c69eae104c441cc7f6a3059dab0e0f6c5
Nathan <nathan.rendall@gmail.com>
2021-06-23 10:36:14 +0100

    inital commit

---
+++ src/pub.py
@@ -0,0 +1,4 @@
+class Pub:
+    def __init__(self, name, pub_total_cash):
+        self.name = name
+        self.total_cash = pub_total_cash
---
+++ tests/pub_test.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.case import TestCase
+from src.pub import *
+
+class TestPub(unittest.TestCase):
+    
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
